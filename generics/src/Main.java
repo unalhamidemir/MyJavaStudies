3eb0bec83bb05b9a902f27aa40a1dd1ef003bea6
ÜNAL HAMİ DEMİR <83436561+unalhamidemir@users.noreply.github.com>
2021-09-10 02:02:25 +0300

    generics
    generics

---
+++ generics/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    MyList<Customer> sehirler=new MyList<Customer>();
+	    sehirler.add(new Customer());
+	    sehirler.add(new Customer());
+    }
+}
---
+++ generics/src/MyList.java
@@ -0,0 +1,9 @@
+public class MyList<T> {
+    public void add(T value){
+
+    }
+
+    public void remove(T value){
+
+    }
+}
