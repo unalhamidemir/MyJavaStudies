9e4b30a8cde3064e2f3813985df2aef5e4d7edc4
ÜNAL HAMİ DEMİR <83436561+unalhamidemir@users.noreply.github.com>
2021-09-10 02:02:10 +0300

    exceptionHandling
    exceptionHandling

---
+++ exceptionHandling/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        try
+        {
+            int[] sayilar = new int[]{1,2,3};
+            System.out.println(sayilar[4]);
+        }catch (StringIndexOutOfBoundsException exception){
+            System.out.println(exception);
+        }catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println(exception);
+        }catch (Exception exception){
+            System.out.println("Loglandı : "+exception);
+        }
+        finally {
+            System.out.println("Her Şekilde Çalışır");
+        }
+
+    }
+}
